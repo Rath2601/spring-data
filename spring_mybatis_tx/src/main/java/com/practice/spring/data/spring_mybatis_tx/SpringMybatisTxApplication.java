5f577ac23f60cbcb72c6474a25f015f445ac9b2d
Rathnasabapathy Ramadoss <rathnasabapathy.ramadoss@pcbank.ca>
2025-12-29 23:10:27 +0530

    feat : "Initial Spring Boot project with Maven Wrapper"

---
+++ spring_mybatis_tx/src/main/java/com/practice/spring/data/spring_mybatis_tx/SpringMybatisTxApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.data.spring_mybatis_tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisTxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisTxApplication.class, args);
+	}
+
+}
---
+++ spring_mybatis_tx/src/test/java/com/practice/spring/data/spring_mybatis_tx/SpringMybatisTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.data.spring_mybatis_tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisTxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
